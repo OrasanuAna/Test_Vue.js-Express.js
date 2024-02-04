8b722c62c776050e91198e408f30330b3ccc58a2
OrasanuAna <orasanu.anamaria19@gmail.com>
2024-02-05 01:12:45 +0200

    Am sters tot, am instalat vue3 & vite & node.js & express.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,21 +1,11 @@
-console.log('1 2 3 4 5 6 7 8 9 10');
-
 const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-
 const app = express();
-app.use(morgan('combined'))
-app.use(express.json());
-app.use(cors());
+const port = 3000;
 
-
-app.post('/register', (req, res) => {
-    res.send({
-        message: 'Hello ${req.body.email}! Your user was registered! Have fun!'
-    });
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.listen(process.env.PORT || 8081);
-
-
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
