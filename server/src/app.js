b2a98f63f71fcb6893a53f84808d0a2db36afc32
OrasanuAna <orasanu.anamaria19@gmail.com>
2024-02-12 00:25:23 +0200

    Api.js + AuthenticationService.js + app.js post/get

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,21 @@
 console.log('Hello Ana!');
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('combined'));
+app.use(express.json());
+app.use(cors());
+
+app.post('/register', (req, res) => {
+  res.send({
+    message: "Hello ${req.body.email}! Your user was registered"
+  });
+});
+
+
+app.listen(process.env.PORT || 8081);
+
+
